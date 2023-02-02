9b390e20927d7bee45796cf7c84b8a2bc9567494
kelvinenc <ingkelvinencarnacion@hotmail.com>
2023-02-02 12:17:56 -0400

    Add context to apple

--- app.js
+++ app.js
@@ -1,3 +1,12 @@
+5 > 4
+"apple" > "pineapple"
+"2" > "12"
+undefined == null
+undefined === null
+null == "\n0\n"
+null === +"\n0\n"
+
+
 5 > 4 // → true
 "apple" > "pineapple" // → false
 "2" > "12" // → true
